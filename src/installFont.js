67c4635f634b123564de27b561c6a19d990a3776
Elise Wei <elise.wei@gmail.com>
2022-05-24 10:05:55 +1200

    Add an access check to font installation

--- src/installFont.js
+++ src/installFont.js
@@ -7,18 +7,25 @@ function installFont(pathToFonts) {
   const pathToFontFile = path.join('resources', fontFileName)
   const pathToFontDestination = path.join(pathToFonts, fontFileName)
 
-  console.log('copying font file...')
-  return copyFile(
-    path.resolve(__dirname, pathToFontFile),
-    pathToFontDestination,
-    constants.COPYFILE_EXCL
-  ).catch(err => {
-    if (err.message.match("file already exists")) {
-      console.log('You already have this font.')
-    } else {
-      console.log("Oh no!", err)
-    }
-  })
+  return access(pathToFonts, constants.W_OK)
+    .then(() => {
+      return copyFile(
+        path.resolve(__dirname, pathToFontFile),
+        pathToFontDestination,
+        constants.COPYFILE_EXCL
+      )
+        .then(() => console.log('✅ Font file copied'))
+        .catch(err => {
+          if (err.message.match("file already exists")) {
+            console.log('✅ You already have this font.')
+          } else {
+            console.log("❗️Oh no!", err)
+          }
+        })
+    }).catch((err) => {
+      console.log('🛑 No permission to write to fonts folder. You can change the permissions or manually copy')
+      console.log(`${path.resolve(__dirname, pathToFontFile)} to\n${pathToFontDestination}`)
+    })
 }
 
 module.exports = installFont
