96b36d1f80de7f5be4d2e2e6a812c71e66a9fbbc
Elise Wei <elise.wei@gmail.com>
2022-05-24 10:15:00 +1200

    What if...async await?

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const installFont = require('./src/installFont')
 const promptUser = require('./src/promptUser')
 const config = require('./userconfig')
 
+const setup = async ({ newCodeProfile, codeAlias, pathToSettings, pathToFonts } = {}) => {
   console.log('----------------------')
-  if (newCodeProfile) {
-    buildNewProfileAndAlias(codeAlias, pathToSettings)
-      .then(() => installFont(pathToFonts))
-      .then(() => installExtensions())
-      .then(() => console.log('Done!'))
-      .catch(console.log)
-  } else {
-    appendOrCreateSettings(pathToSettings)
-      .then(() => installFont(pathToFonts))
-      .then(() => installExtensions())
-      .then(() => console.log('Done!'))
-      .catch(console.log)
+  try {
+    if (newCodeProfile) {
+      await buildNewProfileAndAlias(codeAlias, pathToSettings)
+    } else {
+      await appendOrCreateSettings(pathToSettings)
+    }
+    await installFont(pathToFonts)
+    await installExtensions()
+    console.log('Done!')
+  } catch (err) {
+    console.log(err)
   }
   console.log('----------------------')
 }
