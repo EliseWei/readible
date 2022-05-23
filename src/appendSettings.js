3ede85fe8051684fd99fdd1824c876f8a4c2aba3
Elise Wei <elise.wei@gmail.com>
2022-05-24 11:50:52 +1200

    More async await

--- src/appendSettings.js
+++ src/appendSettings.js
@@ -1,28 +1,31 @@
 const { writeFile, readFile } = require('fs/promises')
 const additionalSettings = require('./resources/additionalSettings')
 
-function appendOrCreateSettings(pathToSettings) {
+const appendOrCreateSettings = async (pathToSettings) => {
   console.group('Creating settings...')
-  return readFile(pathToSettings, { encoding: 'utf-8' })
-    .then(contents => {
-      // This is the append
-      const originalSettings = JSON.parse(contents)
-      const combinedSettings = { ...originalSettings, ...additionalSettings }
-      return writeFile(pathToSettings, JSON.stringify(combinedSettings))
-        .then(() => console.log('✅ Settings added'))
-    })
-    .catch(err => {
-      if (err.message.match('Unexpected token')) {
-        console.log(err, '\n❓ The most likely cause of this is slightly malformed JSON')
-      } else if (err.message.match('no such file')) {
-        console.log('🔎 No settings file found at the specified location. Creating a new one.')
-        // This is the create
-        return writeFile(pathToSettings, JSON.stringify(additionalSettings))
-          .then(() => console.log('✅ Settings added'))
-      } else {
+  try {
+    const contents = await readFile(pathToSettings, { encoding: 'utf-8' })
+    // This is the append
+    const originalSettings = JSON.parse(contents)
+    const combinedSettings = { ...originalSettings, ...additionalSettings }
+    await writeFile(pathToSettings, JSON.stringify(combinedSettings))
+    console.log('✅ Settings added')
+  } catch (err) {
+    if (err.message.match('Unexpected token')) {
+      console.log(err, '\n❓ The most likely cause of this is slightly malformed JSON')
+    } else if (err.message.match('no such file')) {
+      console.log('🔎 No settings file found at the specified location. Creating a new one.')
+      // This is the create
+      try {
+        await writeFile(pathToSettings, JSON.stringify(additionalSettings))
+        console.log('✅ Settings added')
+      } catch (err) {
         console.log('❗️ Oh no!', err)
       }
-    })
-    .finally(() => console.groupEnd())
+    } else {
+      console.log('❗️ Oh no!', err)
+    }
+  }
+  console.groupEnd()
 }
 module.exports = appendOrCreateSettings
