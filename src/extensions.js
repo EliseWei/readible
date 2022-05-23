32ac9d880224dc8a1376a9bb219fd9aca870ed5c
Elise Wei <elise.wei@gmail.com>
2022-05-24 11:41:16 +1200

    Yet more async await

--- src/extensions.js
+++ src/extensions.js
@@ -6,19 +6,19 @@ const extensions = [
   'vscode-icons-team.vscode-icons'
 ]
 
-function installExtensions() {
+const installExtensions = async () => {
   console.group('Installing extensions...')
-  return Promise.all(extensions.map(extension => {
+  await Promise.all(extensions.map(extension => {
     return exec(`code --force --install-extension ${extension}`,
       { shell: process.env.SHELL },
       (error) => {
         if (error) {
           console.log(error)
-          console.log('\x1b[41m\x1b[1m', `⚠️ Failed to install ${extension}`);
+          console.log(`⚠️ Failed to install ${extension}`);
         }
       })
   }))
-    .then(() => console.log('✅ Extensions installed'))
-    .finally(() => console.groupEnd())
+  console.log('✅ Extensions installed')
+  console.groupEnd()
 }
 module.exports = installExtensions
--- src/installFont.js
+++ src/installFont.js
@@ -2,32 +2,39 @@ const { access, copyFile } = require('fs/promises')
 const { constants } = require('fs')
 const path = require('path')
 
-function installFont(pathToFonts) {
+const installFont = async (pathToFonts) => {
   console.group('Attempting to install fonts...')
   const fontFileName = 'OpenDyslexicMono-Regular.otf'
   const pathToFontFile = path.join('resources', fontFileName)
   const pathToFontDestination = path.join(pathToFonts, fontFileName)
 
-  return access(pathToFonts, constants.W_OK)
-    .then(() => {
-      return copyFile(
+  try {
+    await access(pathToFonts, constants.W_OK)
+    try {
+      await copyFile(
         path.resolve(__dirname, pathToFontFile),
         pathToFontDestination,
         constants.COPYFILE_EXCL
       )
-        .then(() => console.log('✅ Font file copied'))
-        .catch(err => {
-          if (err.message.match('file already exists')) {
-            console.log('✅ You already have this font.')
-          } else {
-            console.log('❗️Oh no!', err)
-          }
-        })
-    }).catch((err) => {
+      console.log('✅ Font file copied')
+    } catch (err) {
+      if (err.message.match('file already exists')) {
+        console.log('✅ You already have this font.')
+      } else {
+        console.log('❗️ Oh no!', err)
+      }
+    }
+  } catch (err) {
+    if (err.message.match('no such file or directory')) {
+      console.log(`❗️ ${pathToFonts} does not exist. You can manually copy ${path.resolve(__dirname, pathToFontFile)}, when you're certain where it should go`)
+    } else if (err.message.match('permission denied')) {
       console.log('🛑 No permission to write to fonts folder. You can change the permissions or manually copy')
       console.log(`${path.resolve(__dirname, pathToFontFile)} to\n${pathToFontDestination}`)
-    })
-    .finally(() => console.groupEnd())
+    } else {
+      console.log('❗️ Oh no!', err)
+    }
+  }
+  console.groupEnd()
 }
 
 module.exports = installFont
