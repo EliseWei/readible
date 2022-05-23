129f3d5335558cccc503b31b1b5912b20e8790c9
Elise Wei <elise.wei@gmail.com>
2022-05-24 10:54:21 +1200

    More async await

--- src/newProfile.js
+++ src/newProfile.js
@@ -2,29 +2,33 @@ const appendOrCreateSettings = require('./appendSettings')
 const { mkdir, appendFile, readFile } = require('fs/promises')
 const path = require('path')
 
-function createNewProfile(codeAlias, pathToSettings) {
+const createNewProfile = async (codeAlias, pathToSettings) => {
   const dataPath = path.join(pathToSettings, 'data', 'User')
-  return mkdir(dataPath, { recursive: true })
-    .then(() => {
-      return appendOrCreateSettings(path.join(dataPath, 'settings.json'))
-    })
-    .then(() => {
-      console.group('Creating alias...')
-      const aliasString = `alias ${codeAlias}="code --user-data-dir ${path.join(pathToSettings, 'data')}"\n`
-      const shellConfig = `.${path.basename(process.env.SHELL)}rc`
-      const aliasPath = path.join(process.env.HOME, shellConfig)
-      console.log(`🔎 Looking for config at ${aliasPath}`)
-      return readFile(aliasPath, { encoding: 'utf-8' })
-        .then(contents => {
-          return appendFile(aliasPath, aliasString)
-            .then(() => console.log('✅ Alias created. Restart your terminal.'))
-        })
-        .catch(err => {
-          console.log(`❗️ No config file found at ${aliasPath}`)
-          console.log(`Please add the following to your aliases:\n ${aliasString}`)
-        })
-        .finally(() => console.groupEnd())
-    })
+  try {
+    await mkdir(dataPath, { recursive: true })
+    await appendOrCreateSettings(path.join(dataPath, 'settings.json'))
+  } catch (err) {
+    console.log('Failed to create a new profile\n', err)
+  }
+
+  console.group('Creating alias...')
+  const aliasString = `alias ${codeAlias}="code --user-data-dir ${path.join(pathToSettings, 'data')}"\n`
+  const shellConfig = `.${path.basename(process.env.SHELL)}rc`
+  const aliasPath = path.join(process.env.HOME, shellConfig)
+  console.log(`🔎 Looking for config at ${aliasPath}`)
+  try {
+    await readFile(aliasPath, { encoding: 'utf-8' })
+    try {
+      await appendFile(aliasPath, aliasString)
+      console.log('✅ Alias created. Restart your terminal.')
+    } catch (err) {
+      console.log('❗️Oh no!', err)
+    }
+  } catch (err) {
+    console.log(`❗️ No config file found at ${aliasPath}`)
+    console.log(`Please add the following to your aliases:\n ${aliasString}`)
+  }
+  console.groupEnd()
 }
 
 module.exports = createNewProfile
